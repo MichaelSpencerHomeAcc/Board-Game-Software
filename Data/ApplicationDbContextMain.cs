d352013394cb94c32d2ccab49d1a71f0b0cdb4d3
Michael Spencer <mike_j_spencer@sky.com>
2026-02-15 16:27:59 +0000

    push changes

--- Data/ApplicationDbContextMain.cs
+++ Data/ApplicationDbContextMain.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Board_Game_Software.Data
 {
-    public class ApplicationDbContextMain : IdentityDbContext
+    public class ApplicationDbContextMain
+        : IdentityDbContext<IdentityUser>
     {
-        public ApplicationDbContextMain(DbContextOptions<ApplicationDbContextMain> options)
+        public ApplicationDbContextMain(
+            DbContextOptions<ApplicationDbContextMain> options)
             : base(options)
         {
         }
