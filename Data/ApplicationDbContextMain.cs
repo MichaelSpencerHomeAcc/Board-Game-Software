ef96de187572ff015479d9cad940a1f4a1841ce2
Michael Spencer <mike_j_spencer@sky.com>
2026-02-15 17:04:48 +0000

    Revert "push changes"
    This reverts commit d352013394cb94c32d2ccab49d1a71f0b0cdb4d3.

--- Data/ApplicationDbContextMain.cs
+++ Data/ApplicationDbContextMain.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Board_Game_Software.Data
 {
-    public class ApplicationDbContextMain
-        : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContextMain : IdentityDbContext
     {
-        public ApplicationDbContextMain(
-            DbContextOptions<ApplicationDbContextMain> options)
+        public ApplicationDbContextMain(DbContextOptions<ApplicationDbContextMain> options)
             : base(options)
         {
         }
--- Models/BoardGameDBContext.partial.cs
+++ Models/BoardGameDBContext.partial.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Board_Game_Software.Models;
@@ -8,22 +7,28 @@ public partial class BoardGameDbContext
 {
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
     {
+        // DateOnly? -> date
         var dateOnlyNullableConverter = new ValueConverter<DateOnly?, DateTime?>(
             d => d.HasValue ? d.Value.ToDateTime(TimeOnly.MinValue) : null,
             d => d.HasValue ? DateOnly.FromDateTime(d.Value) : null);
 
+        // DateOnly -> date
         var dateOnlyConverter = new ValueConverter<DateOnly, DateTime>(
             d => d.ToDateTime(TimeOnly.MinValue),
             d => DateOnly.FromDateTime(d));
 
+        // BoardGame.ReleaseDate (nullable)
         modelBuilder.Entity<BoardGame>()
             .Property(e => e.ReleaseDate)
             .HasConversion(dateOnlyNullableConverter)
             .HasColumnType("date");
 
+        // BoardGameNight.GameNightDate (non-nullable)
         modelBuilder.Entity<BoardGameNight>()
             .Property(e => e.GameNightDate)
             .HasConversion(dateOnlyConverter)
             .HasColumnType("date");
+
+        // Add any other DateOnly properties here the same way...
     }
 }
