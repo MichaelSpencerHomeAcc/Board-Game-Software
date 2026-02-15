54ba9b16c9f92476b398276f866c029b7b74fb60
Michael Spencer <mike_j_spencer@sky.com>
2026-02-15 17:04:39 +0000

    Revert "new build"
    This reverts commit 4c928f93885baaf9ab280a87aa10a135fd911635.

---
+++ Models/BoardGameDBContext.partial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Board_Game_Software.Models;
+
+public partial class BoardGameDbContext
+{
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+    {
+        var dateOnlyNullableConverter = new ValueConverter<DateOnly?, DateTime?>(
+            d => d.HasValue ? d.Value.ToDateTime(TimeOnly.MinValue) : null,
+            d => d.HasValue ? DateOnly.FromDateTime(d.Value) : null);
+
+        var dateOnlyConverter = new ValueConverter<DateOnly, DateTime>(
+            d => d.ToDateTime(TimeOnly.MinValue),
+            d => DateOnly.FromDateTime(d));
+
+        modelBuilder.Entity<BoardGame>()
+            .Property(e => e.ReleaseDate)
+            .HasConversion(dateOnlyNullableConverter)
+            .HasColumnType("date");
+
+        modelBuilder.Entity<BoardGameNight>()
+            .Property(e => e.GameNightDate)
+            .HasConversion(dateOnlyConverter)
+            .HasColumnType("date");
+    }
+}
